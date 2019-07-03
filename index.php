38d9448f28367b64ed3b3435d59a1bae444dd1e6
shashikumarvchandru <shashikumarvchandru@yahoo.com>
2019-07-03 18:13:52 +0530

    DATABASE

--- index.php
+++ index.php
@@ -7,5 +7,21 @@
 	Hello World
 
 	Welcome Shashi,
+
+	<?php
+
+		$servername = "sql146.main-hosting.eu.";
+		$username = "u802709898_sampl";
+		$password = "sample123!@#";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+		echo "Connected successfully";
+	?>
 </body>
 </html>
